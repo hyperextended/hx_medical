11d6fa2c90c44d9322578736504fde6a4b10ddac
SumnDaiy <87270976+SumnDaiy@users.noreply.github.com>
2023-01-26 04:10:24 +0100

    refactor stagger.lua
    if female then use female walkanim else use male, fix 'default' anim

--- client/statuses/stagger.lua
+++ client/statuses/stagger.lua
@@ -1,17 +1,8 @@
 PlayerIsStaggered = false
 local intensity = 100
 local prevWalk
-local pedMale
 
-if PlayerIsLoaded and not PlayerIsDead and IsPedMale(cache.ped) then
-    pedMale = true
-    exports.scully_emotemenu:SetWalk('move_m@multiplayer') -- this line can be removed if scully fixes 'deafult'
-    prevWalk = exports.scully_emotemenu:GetCurrentWalk()
-else
-    pedMale = false
-    exports.scully_emotemenu:SetWalk('move_f@multiplayer') -- this line can be removed if scully fixes 'deafult'
-    prevWalk = exports.scully_emotemenu:GetCurrentWalk()
-end
+local walkAnim
 
 local function canTrip(ped)
     if IsPedWalking(ped) or IsPedSwimming(ped) or IsPedClimbing(ped) or IsPedRagdoll(ped) or not IsPedOnFoot(ped) then return false
@@ -23,19 +14,10 @@ local function stagger()
     while PlayerIsStaggered do
         local curWalk = exports.scully_emotemenu:GetCurrentWalk()
         local chance = math.random(100)
-        if pedMale and curWalk ~= 'move_m@injured' then
-            SetPedMoveRateOverride(cache.ped, 100 - (intensity / 5))
-            SetPedMovementClipset(
-                cache.ped,
-                'move_m@injured',
-                0.85)
-        else
-            SetPedMoveRateOverride(cache.ped, 100 - (intensity / 5))
-            SetPedMovementClipset(
-                cache.ped,
-                'move_f@injured',
-                0.85)
+        if curWalk ~= walkAnim then
+            exports.scully_emotemenu:SetWalk(walkAnim)
         end
+        SetPedMoveRateOverride(cache.ped, 100 - (intensity / 5))
         if IsPedSprinting(cache.ped) then chance = chance + 10 end
         if canTrip(cache.ped) and chance > 95 then
             ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', 0.1)
@@ -43,57 +25,28 @@ local function stagger()
         end
         Wait(1000)
     end
-    print('restoring walk')
-    exports.scully_emotemenu:SetWalk(prevWalk)
-end
---[[ local function stagger(amount)
-    prevWalk = exports.scully_emotemenu:GetCurrentWalk()
-    print(prevWalk)
-    PlayerIsStaggered = true
-    while PlayerIsStaggered and (intensity > 0) do
-        local ped = cache.ped
-        local curWalk = exports.scully_emotemenu:GetCurrentWalk()
-        local chance = math.random(100)
-        print(curWalk)
-        if curWalk ~= 'move_m@injured' then
-            SetPedMoveRateOverride(cache.ped, 100 - (intensity / 5))
-            SetPedMovementClipset(
-                cache.ped,
-                'move_injured_generic',
-                0.85)
-        end
-        print(canTrip(ped), chance)
-        if IsPedSprinting(ped) then chance = chance + 10 end
-        if canTrip(ped) and chance > 95 then
-            ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', 0.1)
-            SetPedToRagdollWithFall(ped, 1500, 2000, 1, GetEntityForwardVector(ped), 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-        end
-        Wait(1000)
-    end
-    print('restoring walk')
-    exports.scully_emotemenu:SetWalk(prevWalk)
-end
- ]]
---[[ AddEventHandler('ox:statusTick', function(statuses)
-    if PlayerIsDead or not statuses.stagger then return end
-    if not PlayerIsStaggered then
-        if statuses.stagger > 50 then
-            intensity = statuses.stagger
-            lib.notify({
-                title = 'Medical',
-                description = 'You are staggered!',
-                type = 'error'
-            })
-            stagger(intensity)
-        elseif statuses.stagger == 0 and PlayerIsStaggered then
-            intensity = 0
-            PlayerIsStaggered = false
-        end
+    print('restoring walk', prevWalk)
+    if prevWalk ~= 'default' then
+        exports.scully_emotemenu:SetWalk(prevWalk)
     else
-        intensity = statuses.stagger
+        exports.scully_emotemenu:ResetWalk()
     end
-end) ]]
+    walkAnim = nil
+end
+
+local function MaleFemale()
+    local pedModel = GetEntityModel(cache.ped --[[ Entity ]])
+    if pedModel == 'mp_f_freemode_01' then
+        walkAnim = 'move_f@injured'
+    else
+        walkAnim = 'move_m@injured'
+    end
+end
+
 AddEventHandler('ox:statusTick', function(statuses)
+    if walkAnim == nil then
+        MaleFemale()
+    end
     if PlayerIsDead or not statuses.stagger then return end
     if not PlayerIsStaggered and statuses.stagger > 0 then
         PlayerIsStaggered = true
