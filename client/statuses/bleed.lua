4778be748aea191ddfde1042b22807cae73be1b5
SumnDaiy <87270976+SumnDaiy@users.noreply.github.com>
2023-01-26 05:35:57 +0100

    Refactor bleeding

--- client/statuses/bleed.lua
+++ client/statuses/bleed.lua
@@ -1,33 +1,39 @@
 PlayerIsBleeding = false
 local intensity = 0
-local function bleed(amount)
-    local tickDamage = math.ceil(intensity / 15)
-    SetEntityHealth(cache.ped, GetEntityHealth(cache.ped) - 1)
+local function bleed()
+    while PlayerIsBleeding do
+        local tickTime = (100 - intensity) * 100
+        local tickDamage = 1
+        SetEntityHealth(cache.ped, GetEntityHealth(cache.ped) - tickDamage) 
+        Wait(tickTime)
+    end
 end
 
 AddEventHandler('ox:statusTick', function(statuses)
     if PlayerIsDead or not statuses.bleed then return end
-    if not PlayerIsBleeding then
-        if statuses.bleed > 25 then
-            intensity = statuses.bleed
-            lib.notify({
-                title = 'Medical',
-                description = 'You are bleeding!',
-                type = 'error'
-            })
-            bleed(intensity)
-        elseif statuses.bleed == 0 and PlayerIsBleeding then
-            intensity = 0
-            PlayerIsBleeding = false
-        end
-    else
+    if not PlayerIsBleeding and statuses.bleed > 25 then
+        PlayerIsBleeding = true
+        lib.notify({
+            title = 'Medical',
+            description = 'You are bleeding!',
+            type = 'error'
+        })
         intensity = statuses.bleed
+        bleed()
+    elseif PlayerIsBleeding and statuses.bleed == 0 then
+        intensity = 0
+        PlayerIsBleeding = false
     end
+    intensity = statuses.bleed
 end)
 
-if GetConvar('medical:debug', 'false') == 'true' then
+if GetConvarInt('medical:debug', 0) == 1 then
     RegisterCommand('bleed', function(source, args, rawCommand)
-        TriggerServerEvent('medical:changeStatus', 'limping', tonumber(args[1]))
+        if intensity > 0 then
+            TriggerServerEvent('medical:changeStatus', 'bleed', 0)
+        else
+            TriggerServerEvent('medical:changeStatus', 'bleed', tonumber(args[1]))
+        end
         bleed(args[1])
     end)
 end
