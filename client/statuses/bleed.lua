5c6e2ada166506fabb337910c19d41a1cec50b93
SumnDaiy <87270976+SumnDaiy@users.noreply.github.com>
2023-01-28 18:53:41 +0100

    Add
    Notification is now called with function
    Screen effect red
    Persistent notification test (not working gets stuck on screen)

--- client/statuses/bleed.lua
+++ client/statuses/bleed.lua
@@ -1,29 +1,39 @@
 PlayerIsBleeding = false
 local intensity = 0
+local function ShowNotification()
+    lib.notify({
+        id = "medical_playerbleed",
+        title = 'Medical',
+        description = 'You are bleeding!',
+        type = 'error'
+    })
+    Wait(100)
+end
 
 local function bleed()
     while PlayerIsBleeding do
         local tickTime = (110 - intensity) * 100
         local tickDamage = 1
+        SetTimecycleModifier("glasses_red") -- might not needed if notification is not stuck on screen
+        SetTimecycleModifierStrength(intensity/100) -- might crash the game idk need more test
+        ShowNotification()
         SetEntityHealth(cache.ped, GetEntityHealth(cache.ped) - tickDamage)
         Wait(tickTime)
     end
 end
 
+
 AddEventHandler('ox:statusTick', function(statuses)
     if PlayerIsDead or not statuses.bleed then return end
     if not PlayerIsBleeding and statuses.bleed > 25 then
         PlayerIsBleeding = true
-        lib.notify({
-            title = 'Medical',
-            description = 'You are bleeding!',
-            type = 'error'
-        })
         intensity = statuses.bleed
+        ShowNotification()
         bleed()
     elseif PlayerIsBleeding and statuses.bleed == 0 then
-        intensity = 0
         PlayerIsBleeding = false
+        intensity = 0
+        ClearTimecycleModifier()
     end
     intensity = statuses.bleed
 end)
