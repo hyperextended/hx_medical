35048a0ec6d441d2a3c727f808a56b0fd1af6995
SumnDaiy <sumndaiy@gmail.com>
2023-01-28 22:48:14 -0800

    Update bleed.lua

--- client/statuses/bleed.lua
+++ client/statuses/bleed.lua
@@ -5,27 +5,37 @@ local function bleed()
     while PlayerIsBleeding do
         local tickTime = (110 - intensity) * 100
         local tickDamage = 1
+        SetTimecycleModifier("glasses_red") -- might not needed if notification is not stuck on screen
+        SetTimecycleModifierStrength(intensity/100) -- might crash the game idk need more test
         SetEntityHealth(cache.ped, GetEntityHealth(cache.ped) - tickDamage)
         Wait(tickTime)
     end
 end
 
+
 AddEventHandler('ox:statusTick', function(statuses)
     if PlayerIsDead or not statuses.bleed then return end
     if not PlayerIsBleeding and statuses.bleed > 25 then
         PlayerIsBleeding = true
-        lib.notify({
+        intensity = statuses.bleed
+
+        bleed()
+    elseif PlayerIsBleeding and statuses.bleed == 0 then
+        PlayerIsBleeding = false
+        intensity = 0
+        ClearTimecycleModifier()
+    end
+    intensity = statuses.bleed
+    print(PlayerIsBleeding)
+    if PlayerIsBleeding then
+      lib.notify({
+            id = "medical_playerbleed",
             title = 'Medical',
+            duration = 1000,
             description = 'You are bleeding!',
             type = 'error'
         })
-        intensity = statuses.bleed
-        bleed()
-    elseif PlayerIsBleeding and statuses.bleed == 0 then
-        intensity = 0
-        PlayerIsBleeding = false
     end
-    intensity = statuses.bleed
 end)
 
 if GetConvarInt('medical:debug', 0) == 1 then
