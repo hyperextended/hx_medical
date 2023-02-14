856192f0be5a8515cc5a4a25e0073824e43a3ccd
SumnDaiy <87270976+SumnDaiy@users.noreply.github.com>
2023-02-14 17:56:11 +0100

    chore(server/main.lua): Whitespace and remove unused code

--- server/main.lua
+++ server/main.lua
@@ -5,8 +5,8 @@
 
 -- TODO: REPLACE UNSECURE EVENT WITH SERVER-SIDE LOGIC
 RegisterNetEvent('medical:changeStatus', function(status, value, changeType)
-    -- print(status, value, changeType)
     local player = Ox.GetPlayer(source)
+
     if changeType == 'add' then
         player.addStatus(status, value)
     elseif changeType == 'remove' then
@@ -18,6 +18,7 @@ end)
 
 local function findBed(beds)
     for i = 1, #beds do
+
         if not beds[i].taken then
             return i
         end
@@ -26,37 +27,42 @@ end
 
 local function inPrison(serverId)
     local player = Ox.GetPlayer(serverId)
+
     if player.getState(self).prison then return true else return false end
 end
 
 lib.callback.register('medical:getBed', function()
     local player = Ox.GetPlayer(source)
     local beds = Data.locations.beds
+
     if inPrison(source) then beds = Data.locations.jailbeds end
+
     local index = findBed(beds)
+
     player.getState(self):set('bedIndex', index, true)
     beds[index].taken = true
-    print(json.encode(beds, { indent = true }))
     return beds[index], index
 end)
 
 RegisterNetEvent('medical:releaseBed', function(index)
     if not index then return end
+
     local player = Ox.GetPlayer(source)
     local beds = Data.locations.beds
+
     if inPrison(source) then beds = Data.locations.jailbeds end
+
     beds[index].taken = false
     player.getState(self):set('bedIndex', nil, true)
     print(beds[index].taken)
 end)
 
-AddEventHandler('ox:playerLoaded', function(source, userid, charid)
-    local player = Ox.GetPlayer(source)
-end)
 
 RegisterNetEvent('medical:revive', function(target)
     local player = Ox.GetPlayer(target or source)
+
     if not player then return end
+
     local playerState = player.getState(self)
     playerState:set('dead', false, true)
 end)
@@ -65,8 +71,8 @@ RegisterNetEvent('medical:heal', function(amount, target)
     TriggerClientEvent('medical:heal', target or source, amount or 200)
 end)
 
+--@TODO Add notifications
 lib.addCommand('group.admin', { 'heal' }, function(source, args)
-    print(('healing %s for %s'):format(args.target, args.amount))
     TriggerClientEvent('medical:heal', args.target or source, args.amount or 100)
 end, { 'amount:?number', 'target:?number' })
 
@@ -76,7 +82,9 @@ end, { 'target:?number' })
 
 lib.addCommand('group.admin', { 'kill' }, function(source, args)
     local player = Ox.GetPlayer(args.target or source)
+
     if not player then return end
+
     local playerState = player.getState()
     playerState:set('dead', true, true)
 end, { 'target:?number' })
@@ -84,8 +92,10 @@ end, { 'target:?number' })
 
 lib.addCommand('group.admin', { 'setstatus' }, function(source, args)
     if args.status == nil then return end
+    
     local statuses = { 'bleed', 'unconscious', 'stagger', 'thirst', 'hunger' }
     local player = Ox.GetPlayer(source)
+
     for i = 1, #statuses do
         if statuses[i] == args.status then
             player.setStatus(args.status, tonumber(args.amount) or 100)
--- server/target/target.lua
+++ server/target/target.lua
@@ -1,5 +1,7 @@
 lib.callback.register('medical:curePlayer', function(source, target, status)
   local targetPlayer = Ox.GetPlayer(target)
+  
   if not targetPlayer then return end
+  
   targetPlayer.setStatus(status, 0)
 end)
